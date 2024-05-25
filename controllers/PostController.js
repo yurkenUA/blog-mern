c4d977642784fdb9ac3fe2a966e60a0e3991dbcb
Yurii Zatoka <yuriy.zatoka@gmail.com>
2024-05-24 18:37:13 -0700

    added getting tag's route

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -13,6 +13,24 @@ export const getAll = async (req, res) => {
 	}
 };
 
+export const getLastTags = async (req, res) => {
+	try {
+		const posts = await PostModel.find().limit(5).exec();
+
+		const tags = posts
+			.map((obj) => obj.tags)
+			.flat()
+			.slice(0, 5);
+
+		res.json(tags);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: 'Could not got the tags',
+		});
+	}
+};
+
 export const getOne = async (req, res) => {
 	try {
 		const postId = req.params.id;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import {
 	loginValidation,
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationError, UserController.login);
@@ -45,6 +47,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 });
 
 app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationError, PostController.create);
 app.get('/posts/:id', PostController.getOne);
 app.delete('/posts/:id', checkAuth, PostController.remove);
