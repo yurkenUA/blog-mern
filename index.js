c532da7a5c84adb8ca549b068ea5565eff86a4c2
Yurii Zatoka <yuriy.zatoka@gmail.com>
2024-06-07 14:44:54 -0700

    Creating uploads with fs lib

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import fs from 'fs';
 import multer from 'multer';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -24,6 +25,9 @@ const app = express();
 
 const storage = multer.diskStorage({
 	destination: (_, __, cb) => {
+		if (!fs.existsSync('uploads')) {
+			fs.mkdirSync('uploads');
+		}
 		cb(null, 'uploads');
 	},
 	filename: (_, file, cb) => {
